6fcfa5d40d0ee6af4cfe653db71707aa5ef5eb89
JayHaymes <jjhaymes23@gmail.com>
2024-05-24 18:42:59 -0500

    styled about me section

--- portfolio-react/src/Components/Navbar/Hero/Hero.jsx
+++ portfolio-react/src/Components/Navbar/Hero/Hero.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './Hero.css'
-import profile_img from '../../../assets/profile_img_jeffrey.jpg'
+import profile_img from '../../../assets/profile_img_jeffrey.svg'
 
 const Hero = () => {
   return (
