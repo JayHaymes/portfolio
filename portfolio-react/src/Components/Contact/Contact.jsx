68a1652ad1a45a09abe342aaf43138f3b34edd7b
JayHaymes <jjhaymes23@gmail.com>
2024-05-27 19:17:08 -0500

    contact form onSubmit

--- portfolio-react/src/Components/Contact/Contact.jsx
+++ portfolio-react/src/Components/Contact/Contact.jsx
@@ -1,50 +1,75 @@
-import React from 'react'
-import './Contact.css'
-import theme_pattern from '../../assets/theme_pattern.svg'
-import mail_icon from '../../assets/mail_icon.svg'
-import location_icon from '../../assets/location_icon.svg'
-import call_icon from '../../assets/call_icon.svg'
-import linkedin_icon from '../../assets/linkedin_icon.svg'
-
+import React, { useState } from 'react';
+import './Contact.css';
+import theme_pattern from '../../assets/theme_pattern.svg';
+import mail_icon from '../../assets/mail_icon.svg';
+import location_icon from '../../assets/location_icon.svg';
+import call_icon from '../../assets/call_icon.svg';
+import linkedin_icon from '../../assets/linkedin_icon.svg';
 
 const Contact = () => {
+    const [result, setResult] = useState('');
+
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        setResult('Sending...');
+        const formData = new FormData(event.target);
+    
+        formData.append('access_key', 'b84ba04e-5435-46ec-baf9-65d02eb17ef6');
+    
+        const response = await fetch('https://api.web3forms.com/submit', {
+          method: 'POST',
+          body: formData
+        });
+    
+        const data = await response.json();
+    
+        if (data.success) {
+          setResult('Form Submitted Successfully');
+          event.target.reset();
+        } else {
+          console.log('Error', data);
+          setResult(data.message);
+        }
+      };
+
     return (
         <div id='contact' className='contact'>
-            <div className="contact-title">
+            <div className='contact-title'>
                 <h1>Get in touch</h1>
-                <img src={theme_pattern} alt="" />
+                <img src={theme_pattern} alt='Pattern' />
             </div>
-            <div className="contact-section">
-                <div className="contact-left">
+            <div className='contact-section'>
+                <div className='contact-left'>
                     <h1>Let's talk</h1>
-                    <p>I'm currently available for work, if you're interested in my services you can contact me anytime! </p>
-                    <div className="contact-details">
-                        <div className="contact-detail">
-                            <img src={mail_icon} alt="" /><p>jjhaymes23@gmail.com</p>
+                    <p>I'm currently available for work, if you're interested in my services you can contact me anytime!</p>
+                    <div className='contact-details'>
+                        <div className='contact-detail'>
+                            <img src={mail_icon} alt='Email Icon' /><p>jjhaymes23@gmail.com</p>
                         </div>
-                        <div className="contact-detail">
-                            <img src={call_icon} alt="" /><p>708-420-5577</p>
+                        <div className='contact-detail'>
+                            <img src={call_icon} alt='Call Icon' /><p>708-420-5577</p>
                         </div>
-                        <div className="contact-detail">
-                            <img src={location_icon} alt="" /><p>Chicago, Illinois</p>
+                        <div className='contact-detail'>
+                            <img src={location_icon} alt='Location Icon' /><p>Chicago, Illinois</p>
                         </div>
-                        <div className="contact-detail">
-                            <img src={linkedin_icon} alt="" /><p>https://www.linkedin.com/in/jeffrey-haymes/</p>
+                        <div className='contact-detail'>
+                            <img src={linkedin_icon} alt='LinkedIn Icon' /><p><a href='https://www.linkedin.com/in/jeffrey-haymes/' target='_blank' rel='noopener noreferrer'>https://www.linkedin.com/in/jeffrey-haymes/</a></p>
                         </div>
                     </div>
                 </div>
-                <form className="contact-right">
-                    <label htmlFor="">Your Name</label>
-                    <input type="text" placeholder='Enter your name' name='name' />
-                    <label htmlFor="">Your Email</label>
-                    <input type="email" placeholder='Enter your email' name='email' />
-                    <label htmlFor="">Write your message here</label>
-                    <textarea name="message" rows="8" placeholder='Enter your message'></textarea>
-                    <button type='submit' className="contact-submit">Submit now</button>
+                <form onSubmit={onSubmit} className='contact-right'>
+                    <label htmlFor='name'>Your Name</label>
+                    <input type='text' placeholder='Enter your name' name='name' required />
+                    <label htmlFor='email'>Your Email</label>
+                    <input type='email' placeholder='Enter your email' name='email' required />
+                    <label htmlFor='message'>Write your message here</label>
+                    <textarea name='message' rows='8' placeholder='Enter your message' required></textarea>
+                    <button type='submit' className='contact-submit'>Submit now</button>
+                    <p className='form-result'>{result}</p>
                 </form>
             </div>
         </div>
-    )
+    );
 }
 
-export default Contact
+export default Contact;
