81c3256dc03c4caecc6fddf32516b511aea9a576
JayHaymes <jjhaymes23@gmail.com>
2024-05-27 19:49:37 -0500

    media css styling

--- portfolio-react/src/Components/Navbar/Navbar.jsx
+++ portfolio-react/src/Components/Navbar/Navbar.jsx
@@ -2,12 +2,28 @@ import React from 'react';
 import AnchorLink from 'react-anchor-link-smooth-scroll';
 import './Navbar.css';
 import logo from '../../assets/logo-color.svg';
+import menu_open from '../../assets/menu_open.svg'
+import menu_close from '../../assets/menu_close.svg'
+import { useRef } from 'react';
 
 const Navbar = () => {
+
+    const menuRef = useRef();
+
+    const openMenu = () => {
+        menuRef.current.style.right="0";
+    }
+    const closeMenu = () => {
+        menuRef.current.style.right="-350px";
+    }
+
+
     return (
         <div className='navbar'>
             <img className='navbar-logo' src={logo} alt='Logo' />
-            <ul className="nav-menu">
+            <img src={menu_open} onClick={openMenu} alt="" className='nav-mob-open' />
+            <ul ref={menuRef} className="nav-menu">
+                <img src={menu_close} onClick={closeMenu} alt="" className="nav-mob-close" />
                 <li><AnchorLink className='anchor-link' href='#home'><p>Home</p></AnchorLink></li>
                 <li><AnchorLink className='anchor-link' offset='50' href='#about'><p>About Me</p></AnchorLink></li>
                 <li><AnchorLink className='anchor-link' offset='50' href='#services'><p>Services</p></AnchorLink></li>
