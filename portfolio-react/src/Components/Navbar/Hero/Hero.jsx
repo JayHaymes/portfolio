00afcd5b31ec78a5db8588e8e65621106eef31c9
JayHaymes <jjhaymes23@gmail.com>
2024-05-28 17:39:45 -0500

    touch up css for all components

--- portfolio-react/src/Components/Navbar/Hero/Hero.jsx
+++ portfolio-react/src/Components/Navbar/Hero/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
   return (
     <div id='home' className='hero'>
         <img className='profile-picture' src={profile_img} alt=''></img>
-        <h1><span>I'm Jeffrey Haymes,</span>Fullstack developer based in Illinois.</h1>
+        <h1><span>I'm Jeffrey Haymes,</span> Fullstack developer based in Illinois.</h1>
         <p>I am a dedicated and passionate Fullstack Web Developer who recently graduated from a web development boot camp at the University of Illinois Chicago in collaboration with Fullstack Academy. </p>
         <div className="hero-action">
           <div className="hero-connect"><AnchorLink className='anchor-link' offset='50' href='#contact'>Connect with me</AnchorLink></div>
